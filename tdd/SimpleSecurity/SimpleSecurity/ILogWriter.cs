7ba04ae813076507eadf18ec00e4ca5a9c231815
David Raine <DavidR@sfwltd.co.uk>
2016-09-06 17:20:25 +0100

    Add TDD SimpleSecurity workshop

---
+++ tdd/SimpleSecurity/SimpleSecurity/ILogWriter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSecurity
+{
+    public interface ILogWriter
+    {
+        /// <summary>
+        /// Writes a message to the log
+        /// </summary>
+        /// <param name="message">the message to write</param>
+        void WriteMessage(string message);
+    }
+}
